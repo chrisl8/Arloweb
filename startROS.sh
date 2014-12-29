469e984dbdc820446375c71e4914a3132a08e6c8
Chris L8 <chrisl8@users.noreply.github.com>
2014-12-29 10:36:51 -0600

    Correct source call to use Metatron only

--- startROS.sh
+++ startROS.sh
@@ -8,15 +8,7 @@
 export ROS_MASTER_URI=http://$(/sbin/ifconfig | grep "inet addr"|grep -v 127.0.0.1|awk -F: '{print $2}'|awk '{print $1}'):11311 # Set to laptop IP
 export ROS_HOSTNAME=$(/sbin/ifconfig | grep "inet addr"|grep -v 127.0.0.1|awk -F: '{print $2}'|awk '{print $1}') # Set to THIS machine's IP
 export ROSLAUNCH_SSH_UNKNOWN=1
-# TODO: this is a mess because we don't know which packages were installed by the user.
-# Which should be taken care of by metatron dependencies!
-# TODO: We also don't know where this might be?!
-if [ -e ~/hector_navigation/devel/setup.bash ]
-then
-  source ~/hector_navigation/devel/setup.bash
-else
-  source ~/metatron/devel/setup.bash
-fi
+source ~/metatron/devel/setup.bash
 /opt/ros/indigo/bin/rosnode list|grep rosout
 if [ $? -ne 0 ]
 then
