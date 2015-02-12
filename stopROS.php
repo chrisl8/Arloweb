903af5eb4aa72c347f50a2c955686be7e15f67f4
Chris L8 <chrisl8@users.noreply.github.com>
2015-02-12 15:25:40 -0600

    Background ROS stop process to improve stability and ensure Apache processes are released.

--- stopROS.php
+++ stopROS.php
@@ -11,7 +11,7 @@ $ini_array = parse_ini_file("arloweb.ini");
 #print_r($ini_array);
 #echo("{$ini_array['scriptFolder']}<br/>");
 #echo("{$ini_array['rosUser']}<br/>");*/
-shell_exec("{$ini_array['scriptFolder']}/stubStopper.sh {$ini_array['scriptFolder']} {$ini_array['rosUser']}");
+shell_exec("nohup {$ini_array['scriptFolder']}/stubStopper.sh {$ini_array['scriptFolder']} {$ini_array['rosUser']} &");
 echo 'Stopping ROS';
 } else {
 echo 'ERROR: arloweb.ini not found . . . ';
